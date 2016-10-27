dae438a6ed765f0966072e3a89de0b0b393e2cb5
Nathan Ladd <nathan@obsidianexchange.com>
2016-10-27 11:28:21 -0500

    Telemetry::Logger AdHoc use is removed

--- test/interactive/interactive_init.rb
+++ test/interactive/interactive_init.rb
@@ -5,11 +5,8 @@ require_relative '../test_init'
 def get_stream_name(file: nil)
   file ||= "tmp/stream_name"
 
-  __logger.trace "Getting stream name (File: #{file})"
-
   if File.exist? file
     stream_name = File.read file
-    __logger.debug "Read stream name from disk (File: #{file}, StreamName: #{stream_name}"
     stream_name
   else
     if ENV['CATEGORY'] == 'on'
@@ -22,10 +19,7 @@ def get_stream_name(file: nil)
 
     at_exit do
       File.unlink file
-      __logger.debug "Unlinked stream name file (File: #{file}, StreamName: #{stream_name})"
     end
-
-    __logger.debug "Generated stream name (File: #{file}, StreamName: #{stream_name})"
 
     stream_name
   end
