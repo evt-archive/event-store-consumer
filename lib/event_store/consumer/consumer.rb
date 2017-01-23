abfbb0dd491b4d233d3d7338b73c216d6c58c2cb
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-23 16:47:37 -0600

    Vestigal code is removed

--- lib/event_store/consumer/consumer.rb
+++ lib/event_store/consumer/consumer.rb
@@ -4,42 +4,33 @@ module EventStore
       return if cls == Object
 
       cls.class_exec do
-        include ::Log::Dependency
-
-        extend ::Consumer::Build
-        extend ::Consumer::Run
-
-        extend ::Consumer::PositionStoreMacro
+        include ::Consumer
 
         prepend Configure
 
-        initializer :stream
-
-        attr_writer :position_update_interval
-
-        attr_accessor :cycle_timeout_milliseconds
-        attr_accessor :cycle_maximum_milliseconds
-
-        dependency :position_store, ::Consumer::PositionStore
-
+        extend DispatcherMacro
 
         extend Start
-
-        include Module
-
-        extend DispatcherMacro
 
         dependency :subscription, Subscription
         dependency :dispatcher, Dispatcher
         dependency :messaging_dispatcher, EventStore::Messaging::Dispatcher
-        dependency :session, EventSource::EventStore::HTTP::Session
-
-        attr_writer :stream_name
       end
     end
 
-    def error_raised(error, _)
-      raise error
+    def start
+      logger.trace "Starting consumer (StreamName: #{stream.name}, Dispatcher: #{messaging_dispatcher.class})"
+
+      Actor::Start.(dispatcher)
+      Actor::Start.(subscription)
+
+      logger.info "Consumer started (StreamName: #{stream.name}, Dispatcher: #{messaging_dispatcher.class})"
+
+      return subscription, dispatcher
+    end
+
+    def consumer_stream_name
+      StreamName.consumer_stream_name stream.name
     end
 
     def position_update_interval
@@ -58,7 +49,9 @@ module EventStore
           starting_position = position_store.get
         end
 
-        session = EventSource::EventStore::HTTP::Session.configure self, session: session
+        session ||= EventSource::EventStore::HTTP::Session.build
+
+        stream_name = EventSource::EventStore::HTTP::StreamName.canonize stream.name
 
         messaging_dispatcher = self.class.messaging_dispatcher_class.configure self, attr_name: :messaging_dispatcher
         error_handler = method(:error_raised).to_proc
@@ -82,38 +75,6 @@ module EventStore
       end
     end
 
-    module Module
-      def start
-        logger.trace "Starting consumer (StreamName: #{stream_name}, Dispatcher: #{messaging_dispatcher.class})"
-
-        Actor::Start.(dispatcher)
-        Actor::Start.(subscription)
-
-        logger.info "Consumer started (StreamName: #{stream_name}, Dispatcher: #{messaging_dispatcher.class})"
-
-        return subscription, dispatcher
-      end
-
-      def stream_name
-        @stream_name ||= EventSource::EventStore::HTTP::StreamName.canonize stream.name
-      end
-
-      def consumer_stream_name
-        StreamName.consumer_stream_name stream_name
-      end
-    end
-
-    module CategoryMacro
-      def category_macro(category)
-        category = Casing::Camel.(category, symbol_to_string: true)
-
-        define_singleton_method :stream_name do
-          EventSource::EventStore::HTTP::StreamName.category_stream_name category
-        end
-      end
-      alias_method :category, :category_macro
-    end
-
     module DispatcherMacro
       def dispatcher_macro(dispatcher_class)
         define_singleton_method :messaging_dispatcher_class do
@@ -131,17 +92,6 @@ module EventStore
 
         return instance, *actors
       end
-    end
-
-    module StreamMacro
-      def stream_macro(stream)
-        stream = Casing::Camel.(stream, symbol_to_string: true)
-
-        define_singleton_method :stream_name do
-          stream
-        end
-      end
-      alias_method :stream, :stream_macro
     end
   end
 end
--- test/automated/consumer/consumer_is_started.rb
+++ test/automated/consumer/consumer_is_started.rb
@@ -3,13 +3,15 @@ require_relative '../automated_init'
 context "Consumer is Started" do
   consumer_class = Controls::Consumer::Example
 
-  consumer = consumer_class.build 'someStream'
+  session = EventSource::EventStore::HTTP::Session.build
+
+  consumer = consumer_class.build 'someStream', session: session
 
   subscription, dispatcher = consumer.start
 
   test "Session is supplied to subscription" do
     assert subscription do
-      session? consumer.session
+      session? session
     end
   end
 
