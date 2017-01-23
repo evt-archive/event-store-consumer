c67e9e35a8a22c0ae857739c99bc7cca714b7149
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-23 17:52:36 -0600

    Vestigal controls and tests are removed

--- lib/event_store/consumer/controls.rb
+++ lib/event_store/consumer/controls.rb
@@ -1,28 +1,15 @@
 require 'messaging/event_store/controls'
 
-require 'event_store/consumer/controls/id'
-
-require 'event_store/consumer/controls/address'
 require 'event_store/consumer/controls/category'
-require 'event_store/consumer/controls/error'
-require 'event_store/consumer/controls/no_stream_delay_duration'
+require 'event_store/consumer/controls/id'
+require 'event_store/consumer/controls/message'
 require 'event_store/consumer/controls/position'
 require 'event_store/consumer/controls/stream_name'
 
-require 'event_store/consumer/controls/event_data'
-require 'event_store/consumer/controls/batch'
-
-require 'event_store/consumer/controls/message'
-require 'event_store/consumer/controls/messages/get_batch'
-require 'event_store/consumer/controls/messages/dispatch_event'
-require 'event_store/consumer/controls/messages/enqueue_batch'
 require 'event_store/consumer/controls/messages/consumer_updated'
-
-require 'event_store/consumer/controls/messaging_dispatcher/handler'
-require 'event_store/consumer/controls/messaging_dispatcher'
 require 'event_store/consumer/controls/consumer_stream/write'
-require 'event_store/consumer/controls/position_store'
-require 'event_store/consumer/controls/read'
-require 'event_store/consumer/controls/write'
 
-require 'event_store/consumer/controls/consumer'
+require 'event_store/consumer/controls/dispatcher/handler'
+require 'event_store/consumer/controls/dispatcher'
+
+require 'event_store/consumer/controls/read'
--- lib/event_store/consumer/controls/time.rb
+++
@@ -1,7 +0,0 @@
-module EventStore
-  module Consumer
-    module Controls
-      Time = Clock::Controls::Time
-    end
-  end
-end
